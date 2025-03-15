8b6e46c0af91635a11125d765e7101b32050dcd8
gandis0713 <gandis0713@gmail.com>
2025-03-15 16:54:02 +0900

    prepare june api

--- june/june/june.cpp
+++ june/june/june.cpp
@@ -6,12 +6,12 @@ namespace june
 extern JuneProc procGetProcAddress(char const* procName);
 extern JuneInstance procCreateInstance(JuneInstanceDescriptor const* desc);
 extern JuneApiContext procCreateApiContext(JuneInstance innstance, JuneApiContextDescriptor const* desc);
-// extern void procDestroyInstance(JuneInstance instance);
-// extern void procDestroyApiContext(JuneApiContext context);
-// extern JuneBuffer procCreateBuffer(JuneApiContext context, JuneBufferDescriptor const* descriptor);
-// extern JuneTexture procCreateTexture(JuneApiContext context, JuneTextureDescriptor const* descriptor);
-// extern void procDestroyBuffer(JuneBuffer buffer);
-// extern void procDestroyTexture(JuneTexture texture);
+extern void procDestroyInstance(JuneInstance instance);
+extern void procDestroyApiContext(JuneApiContext context);
+extern JuneBuffer procCreateBuffer(JuneApiContext context, JuneBufferDescriptor const* descriptor);
+extern JuneTexture procCreateTexture(JuneApiContext context, JuneTextureDescriptor const* descriptor);
+extern void procDestroyBuffer(JuneBuffer buffer);
+extern void procDestroyTexture(JuneTexture texture);
 
 } // namespace june
 
@@ -34,33 +34,33 @@ extern "C"
         return procCreateApiContext(innstance, desc);
     }
 
-    // JUNE_EXPORT void juneDestroyInstance(JuneInstance instance)
-    // {
-    //     return procDestroyInstance(instance);
-    // }
+    JUNE_EXPORT void juneDestroyInstance(JuneInstance instance)
+    {
+        return procDestroyInstance(instance);
+    }
 
-    // JUNE_EXPORT void juneDestroyApiContext(JuneApiContext context)
-    // {
-    //     return procDestroyApiContext(context);
-    // }
+    JUNE_EXPORT void juneDestroyApiContext(JuneApiContext context)
+    {
+        return procDestroyApiContext(context);
+    }
 
-    // JUNE_EXPORT JuneBuffer juneCreateBuffer(JuneApiContext context, JuneBufferDescriptor const* descriptor)
-    // {
-    //     return procCreateBuffer(context, descriptor);
-    // }
+    JUNE_EXPORT JuneBuffer juneCreateBuffer(JuneApiContext context, JuneBufferDescriptor const* descriptor)
+    {
+        return procCreateBuffer(context, descriptor);
+    }
 
-    // JUNE_EXPORT JuneTexture juneCreateTexture(JuneApiContext context, JuneTextureDescriptor const* descriptor)
-    // {
-    //     return procCreateTexture(context, descriptor);
-    // }
+    JUNE_EXPORT JuneTexture juneCreateTexture(JuneApiContext context, JuneTextureDescriptor const* descriptor)
+    {
+        return procCreateTexture(context, descriptor);
+    }
 
-    // JUNE_EXPORT void juneDestroyBuffer(JuneBuffer buffer)
-    // {
-    //     return procDestroyBuffer(buffer);
-    // }
+    JUNE_EXPORT void juneDestroyBuffer(JuneBuffer buffer)
+    {
+        return procDestroyBuffer(buffer);
+    }
 
-    // JUNE_EXPORT void juneDestroyTexture(JuneTexture texture)
-    // {
-    //     return procDestroyTexture(texture);
-    // }
+    JUNE_EXPORT void juneDestroyTexture(JuneTexture texture)
+    {
+        return procDestroyTexture(texture);
+    }
 }
--- june/june/proc_table.cpp
+++ june/june/proc_table.cpp
@@ -2,8 +2,8 @@
 
 #include "june/native/instance.h"
 
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 namespace june
 {
@@ -13,10 +13,43 @@ JuneInstance procCreateInstance(JuneInstanceDescriptor const* descriptor)
     return reinterpret_cast<JuneInstance>(Instance::create(descriptor));
 }
 
+void procDestroyInstance(JuneInstance instance)
+{
+    if (instance)
+        delete reinterpret_cast<Instance*>(instance);
+}
+
 JuneApiContext procCreateApiContext(JuneInstance innstance, JuneApiContextDescriptor const* desc)
 {
     return nullptr;
     // return reinterpret_cast<JuneApiContext>(reinterpret_cast<Instance*>(innstance)->createApiContext(desc));
+}
+
+void procDestroyApiContext(JuneApiContext context)
+{
+    // reinterpret_cast<ApiContext*>(context)->destroy();
+}
+
+JuneBuffer procCreateBuffer(JuneApiContext context, JuneBufferDescriptor const* descriptor)
+{
+    return nullptr;
+    // return reinterpret_cast<JuneBuffer>(reinterpret_cast<ApiContext*>(context)->createBuffer(descriptor));
+}
+
+void procDestroyBuffer(JuneBuffer buffer)
+{
+    // reinterpret_cast<Buffer*>(buffer)->destroy();
+}
+
+JuneTexture procCreateTexture(JuneApiContext context, JuneTextureDescriptor const* descriptor)
+{
+    return nullptr;
+    // return reinterpret_cast<JuneTexture>(reinterpret_cast<ApiContext*>(context)->createTexture(descriptor));
+}
+
+void procDestroyTexture(JuneTexture texture)
+{
+    // reinterpret_cast<Texture*>(texture)->destroy();
 }
 
 namespace
